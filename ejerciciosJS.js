18708d75ff7c912f3deeb1a374f696a2d72ec1f0
Reynaldo Munoz <reynaldo.munozf21@gmail.com>
2022-04-08 11:06:07 +0200

    carga segundo ejercicio

--- ejerciciosJS.js
+++ ejerciciosJS.js
@@ -1,3 +1,21 @@
-//EJERCICIO #1
+//EJERCICIO #Tema 1
 
 console.log("Estoy realizando el curso de JavaScript, espero aprender mucho")
+
+//EJERCICIO #Tema 2 
+
+let str = "Esta es la Frase Original";
+
+const cambio = () => { 
+    let str = "Esta es la Frase nueva";
+    console.log (`se cambio la frase a  ${str}`);
+}
+
+cambio();
+console.log (str)
+
+//EJERCICIO #Tema 2 
+//EJERCICIO #Tema 2 
+//EJERCICIO #Tema 2 
+//EJERCICIO #Tema 2 
+//EJERCICIO #Tema 2 
